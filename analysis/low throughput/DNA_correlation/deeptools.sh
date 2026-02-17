141a2720aadf482222b4f778db6905f283ef2515
Zhongyvlong <532798757@qq.com>
2026-02-17 13:57:09 +0800

    Update deeptools.sh

--- analysis/low throughput/DNA_correlation/deeptools.sh
+++ analysis/low throughput/DNA_correlation/deeptools.sh
@@ -1,7 +1,6 @@
-
 #K562 H3K9me3
 multiBigwigSummary BED-file \
-    --BED /data/ass/genome_file/genomic_element_annotation/hg38/het.call.human.K562.hg38.bed \
+    --BED het.call.human.K562.hg38.bed \
     -b K562_H3K9me3_merged_scNanoTriCK-TileSize-100-normMethod-nFrags-ArchR.bw \
        GSM7964462_K562_H3K9me3_scNanoSeq_CUT-Tag.bw \
        ENCFF600IBI_K562_H3K9me3_hg38.bigWig \
@@ -9,7 +8,6 @@ multiBigwigSummary BED-file \
     -out K562_ChIP-seq_H3K9me3_peak_region_correlation.npz \
     -p 60 \
     --outRawCounts K562_ChIP-seq_H3K9me3_peak_region_correlation_cor.tab
-
 
 
 #K562 H3K27me3
@@ -22,6 +20,38 @@ multiBigwigSummary BED-file \
     -out K562_ChIP-seq_H3K27me3_peak_region_correlation.npz \
     -p 60 \
     --outRawCounts K562_ChIP-seq_H3K27me3_peak_region_correlation_cor.tab
+
+#GM12878 H3K9me3
+multiBigwigSummary BED-file \
+    --BED heterochromatin_domain_bins_100kb.bed \
+    -b GM12878_H3K9me3_merged_scNanoTriCK-TileSize-100-normMethod-nFrags-ArchR.bw \
+       GSM7964476_GM12878_H3K9me3.bw \
+       ENCFF349VRN_log2_enrichment_over_Input_5kb.bigwig \
+    --labels GM12878_H3K9me3_scNanoTriCK_merged GM12878_H3K9me3_scNanoSeq_CUT-Tag ChIP-seq_H3K9me3 \
+    -out GM12878_H3K9me3_heterochromatin_domain_correlation.npz \
+    -p 60 \
+    --outRawCounts GM12878_H3K9me3_heterochromatin_domain_correlation_cor.tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
