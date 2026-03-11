4e16f6505f1eaa81eaa9e3707ee8d458c49fe449
Zhongyvlong <532798757@qq.com>
2026-03-11 20:11:55 +0800

    Create genmap.sh

---
+++ analysis/genome  mappability/genmap.sh
@@ -0,0 +1,44 @@
+genmap index -F hg38.fa -I genmap_index/hg38/
+
+genmap map -K 150 -E 2 -I genmap_index/hg38/ \
+	-O 150-kmer/hg38_mappability \
+	-t -w -bg
+
+genmap map -K 3000 -E 4 -I genmap_index/hg38/ \
+	-O /3000-kmer/hg38_mappability \
+	-t -w -bg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
